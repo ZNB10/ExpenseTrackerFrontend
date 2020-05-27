9199652ef88daf0ee47a4828d9ae74da024a33d3
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-27 17:22:38 -0600

    Creacion de componente ThingBox para Dashboard

--- frontend/src/Components/Pages/Private/Dashboard/Dashboard.js
+++ frontend/src/Components/Pages/Private/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Dashboard.css';
+import ThingBox from './ThingsBox';
 
 const BigCard=({...props})=>(<div className="card big">{props.children}</div>);
 const SmallCard=({...props})=>(<div className="card">{props.children}</div>);
@@ -11,7 +12,10 @@ export default class Dashboard extends Component{
             <section>
                 <h1>One Big Thing Dashbord</h1>
                 <section className="main cardHolder fix640">
-                <div>1</div>
+                <ThingBox thingType="big">1</ThingBox>
+                <div className="dataPanel"><span>&lt;--</span><span>2020-05-27</span><span>--&gt;</span></div>
+                <div className="card">2</div>
+                <div className="card">3</div>
                 </section>
             </section>
 

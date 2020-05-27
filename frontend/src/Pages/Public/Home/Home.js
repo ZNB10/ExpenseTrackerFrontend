cb68855b6d2d76118181500e11f3e777470ce695
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-27 04:14:53 -0600

    Cracion de estructura frentend con react

---
+++ frontend/src/Pages/Public/Home/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Button from  '../../../Components/Btns/Buttons';
+
+export default classs Home extends Component {
+    render(){
+        return(
+        <div>
+            <h1>Home Page</h1>
+            <button/>
+        </div>
+        );
+    }
+};

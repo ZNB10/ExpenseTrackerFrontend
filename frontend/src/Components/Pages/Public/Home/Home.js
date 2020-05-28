13d1a9a6a3b0a2372792aa6e2166b34feb0dd8dd
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-28 08:53:40 -0600

    Previsualizacion de como quedaria

--- frontend/src/Components/Pages/Public/Home/Home.js
+++ frontend/src/Components/Pages/Public/Home/Home.js
@@ -8,6 +8,14 @@ export default class Home extends Component {
         return(
             <div className="home"> 
                 <h1>Expense Tracker</h1>
+                <h2>Saldo Inicial : L.5000</h2>
+                <h2>Saldo Actual : L.3000</h2>
+                <h2>Ultimos Gastos</h2>
+                <ul>
+                    <li>Supermercado</li>
+                    <li>Farmacia</li>
+                    <li>Mascota</li>
+                </ul>
                 <div>&nbsp;</div>
                 {(this.props.auth.logged) ? (<div className="half"><Button customClass="primary" onClick={(e) => { this.props.setUnAuth(false)}}><IoIosLogOut/>&nbsp;Logout</Button></div>):null}
             </div>

f7171071d2fecff971c78cb622434785d0e3c90f
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-27 23:19:22 -0600

    Implementacion de Private Route en NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { setJWT } from './Utilities';
 import NavBar from './Components/Common/NavBar/NavBar';
+import PrivateRoute from './PrivateRoute';
 
 import Home from './Components/Pages/Public/Home/Home';
 import Login from './Components/Pages/Public/Login/Login';
@@ -24,24 +25,32 @@ class App extends Component {
     this.setAuth = this.setAuth.bind(this);
   }//contructor
 
+  setUnAuth(error){
+    this.setAuth(false,{});
+  }
+
   setAuth(token, user){
     setJWT(token);
+    let _auth = {
+      logged: token && true,
+      token:token,
+      user:user
+    };
+    setLocalStorage('auth', JSON.stringify(_auth));
     this.setState({
-      auth:{
-        logged: token && true,
-        token: token,
-        user: user
-      }
+      auth: _auth
     });
   }
   render(){
+    console.log(this.state.auth);
     return (
       <Router>
         <section className="container">
-          <Route path="/" exact render={ ()=>(<Login auth={this.state.auth} setAuth={this.setAuth}/>) } />
-          <Route path="/signin" exact component={Sigin}></Route>
-          <Route path="/main" exact component={Dashboard}></Route>
-          <NavBar/>
+          <Route path="/" exact render={ (props)=>(<Home {...props} auth={this.state.auth} setUnAuth={this.setUnAuth}/>) } />
+          <Route path="/login" render={ (props)=>(<Login {...props} auth={this.state.auth} setAuth={this.setAuth}/>)} />
+          <Route path="/signin" component={Sigin}/>
+          <PrivateRoute path="/main" auth={this.state.auth} component={Dashboard}/>
+          <NavBar auth={this.state.auth}/>
         </section>
       </Router>
     );
--- frontend/src/Components/Common/NavBar/NavBar.js
+++ frontend/src/Components/Common/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './NavBar.css';
-import { IoIosLogIn, IoIosHome, IoIosKey } from 'react-icons/io';
+import { IoIosLogIn, IoIosHome, IoIosKey, IoIosList, IoIosToday } from 'react-icons/io';
 
 const NavItem = ({to, children, ...rest})=>{
     return(
@@ -9,12 +9,22 @@ const NavItem = ({to, children, ...rest})=>{
     );
 };
 
-export default ()=>{
-    return (
-        <nav>
-            <NavItem to="/"><IoIosLogIn/>&nbsp;Login</NavItem>
-            <NavItem to="/signin"><IoIosKey/>&nbsp;Signin</NavItem>
-            <NavItem to="/main"><IoIosHome/>&nbsp;Main</NavItem>
-        </nav>
-    );
+export default ({auth, unSetAuth})=>{
+    console.log(auth);
+    if(!auth.logged){
+        return(
+            <nav>
+              <NavItem to="/login"><IoIosLogIn/>&nbsp;Login</NavItem>
+              <NavItem to="/signin"><IoIosKey/>&nbsp;SignIn</NavItem>
+            </nav>
+          )
+    }else{
+        return (
+            <nav>
+                <NavItem to="/"><IoIosHome/>&nbsp;Home</NavItem>
+                <NavItem to="/main"><IoIosToday/>&nbsp;Main</NavItem>
+                <NavItem to="/backlog"><IoIosList/>&nbsp;BackLog</NavItem>
+            </nav>
+        );
+    }
 }

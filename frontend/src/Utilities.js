8cad57df9955aae662820e410f7743d615193064
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-27 23:26:35 -0600

    Manejador de UnAuthHandler

--- frontend/src/Utilities.js
+++ frontend/src/Utilities.js
@@ -15,5 +15,28 @@ export const setJWT = (jwt) =>{
     privateAxios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
 }
 
+export const setUnAuthHandler = (handler) =>{
+    privateAxios.interceptors.response.use(
+        (response)=>{
+            return response;
+        },
+        (error)=>{
+            if(error.response){
+                console.log(error.response.status);
+                switch(error.response.status){
+                    case 401:
+                        handler(error.response.data);
+                    break;
+                }
+                
+            }else if(error.request){
+                console.log(erros.request);
+            }else{
+                console.log(error);
+            }
+            return Promise.reject(error);
+        }
+    );
+}
 export const naxios = normalAxios;
 export const paxios = privateAxios;

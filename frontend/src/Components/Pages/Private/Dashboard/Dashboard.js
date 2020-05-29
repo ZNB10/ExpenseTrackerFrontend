f2733ea3e590e438d8824fdc9b984b52134b1a08
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-28 19:52:39 -0600

    Creacion del formulario 2

--- frontend/src/Components/Pages/Private/Dashboard/Dashboard.js
+++ frontend/src/Components/Pages/Private/Dashboard/Dashboard.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import './Dashboard.css';
-import {MdAdd as Plus} from 'react-icons/md';
 import {paxios} from "../../../../Utilities";
 
 //Common
 import Select from '../../../Common/Select/Select';
 import Campo from '../../../Common/Campo/Campo';
 
-import ThingBox from './ThingsBox';
-import DatePanel from './DatePanel';
 
 /*
 const BigCard=({...props})=>(<div className="card big">{props.children}</div>);
@@ -20,48 +17,24 @@ export default class Dashboard extends Component{
     constructor(){
         super();
         this.state ={
-            things:[],
-            hasMore:true,
-            page:1,
-            itemsToLoad:10
+            expenseDesc:'',
+            expenseMoney:0,
+            error:false
         }
-        this.SelectClick = this.SelectClick.bind(this);
+        this.onChangeHandler = this.onChangeHandler.bind(this);
     }
-    componentDidMount(){
-        paxios.get('/api/things/')
-            .then(({data, status})=>{
-                console.log(data);
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-        ;
-    }
-    SelectClick(page){
-        const items = this.state.itemsToLoad;
-        const uri = `/api/things/page/${{page}}/${items}`;
-        console.log("Uri   "+ uri);
-        paxios.get(uri)
-            .then(
-                ({data})=>{
-                    var numOption = data.length;
-                }
-            )
-            .catch(
-                (err)=>{
-                    console.log(err);
-                }
-            );
-
+    onChangeHandler(e){
+        const{name, value} = e.target;
+        this.setState({...this.state, [name]:value}); 
     }
     render(){
         return(
             <section>
                 <h1>What will I spend today?</h1>
-                <section className="main cardHolder fix640">
+                <section className="main fix640">
                     <form>
                         <label>Choose an expense </label>
-                        <Select onClick={this.SelectClick(3)}>
+                        <Select >
                             <option value={"health"}>Health</option>
                             <option value={"comestible"}>Comestible</option>
                             <option value={"family"}>Family</option>
@@ -70,10 +43,23 @@ export default class Dashboard extends Component{
                             <option value={"Transport"}>Transport</option>
                             <option value={"presents"}>Presents</option>
                             <option value={"purchases"}>Purchases</option>
-                        </Select><br/>
-                        <label>Expense Description</label>
-                        <input></input>
-
+                        </Select><br/><br/>
+                        <Campo
+                            caption="Expense Description"
+                            value={this.state.expenseDesc}
+                            name="expenseDesc"
+                            onChange={this.onChangeHandler}
+                        />
+                         {(this.state.error && true)? (<div className="error">{this.state.error}</div>):null}
+                         <Campo
+                            caption="How much i spent?"
+                            value={this.state.expenseMoney}
+                            name="expenseMoney"
+                            type="number"
+                            onChange={this.onChangeHandler}
+                        />
+                         {(this.state.error && true)? (<div className="error">{this.state.error}</div>):null}
+                        
                     </form>
                 </section>
             </section>

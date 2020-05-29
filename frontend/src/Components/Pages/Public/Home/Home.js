c77cf6c5cdf7756cfe8b310701198749f9ab568e
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-28 19:14:46 -0600

    Arreglo de css del boton logout

--- frontend/src/Components/Pages/Public/Home/Home.js
+++ frontend/src/Components/Pages/Public/Home/Home.js
@@ -7,7 +7,10 @@ export default class Home extends Component {
     render(){
         return(
             <div className="home"> 
-                <h1>Expense Tracker</h1>
+                <h1>
+                    Expense Tracker
+                    {(this.props.auth.logged) ? (<div className="half"><Button customClass="logout" onClick={(e) => { this.props.setUnAuth(false)}}><IoIosLogOut/></Button></div>):null}
+                </h1>
                 <h2>Saldo Inicial : L.5000</h2>
                 <h2>Saldo Actual : L.3000</h2>
                 <h2>Ultimos Gastos</h2>
@@ -17,7 +20,6 @@ export default class Home extends Component {
                     <li>Mascota</li>
                 </ul>
                 <div>&nbsp;</div>
-                {(this.props.auth.logged) ? (<div className="half"><Button customClass="primary" onClick={(e) => { this.props.setUnAuth(false)}}><IoIosLogOut/>&nbsp;Logout</Button></div>):null}
             </div>
         );
     }

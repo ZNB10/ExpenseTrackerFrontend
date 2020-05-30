d11f3d70d8b10bceb5af3811f2c2964a4b37ece4
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-30 17:09:44 -0600

    Consulta en que mas gastas terminado

--- frontend/src/Components/Pages/Public/Home/Home.js
+++ frontend/src/Components/Pages/Public/Home/Home.js
@@ -8,24 +8,20 @@ export default class Home extends Component {
     constructor(){
         super();
         this.state={
-            expenseType: "",
-            expenseDesc: "",
-            expenseMoney:0
+            expenses:[]
         }
-        this.loadMore = this.loadMore.bind(this);
     }
 
-    loadMore(Page){
-        console.log("Holaaaaaaaaaaaaaaa");
+    componentDidMount(){
         const uri = `/api/expenses/expenses/query1`;
         paxios.get(uri)
         .then(
             ({data})=>{
-                const {expenseType, expenseDesc, expenseMoney} = data;
-                const loadedQuery = this.state.expenseType;
-                expenseType.map((e)=>loadedQuery.push(e));
-                for (const key in data.expenseType) {
-                    console.log(data.expenseType[key]);
+                const {expenses} = data;
+                const loadedExpenses = this.state.expenses;
+                expenses.map((e)=>loadedExpenses.push(e));
+                for (const key in this.state.expenses) {
+                    console.log(this.state.expenses[key]);
                 }   
             }
         )
@@ -37,7 +33,17 @@ export default class Home extends Component {
     }
 
     render(){
-        this.loadMore();
+        const items = this.state.expenses.map(
+            (expens)=>{
+                return(
+                    <div key={expens._id}>
+                        <span>
+                            Hi, you spend a lot on {expens.expenseDesc}&nbsp;
+                            you spent {expens.expenseMoney}$</span>
+                    </div>
+                );
+            }
+        )
         return(
             <div className="home"> 
                 <h1>
@@ -45,15 +51,9 @@ export default class Home extends Component {
                     
                     {(this.props.auth.logged) ? (<div className="half"><Button customClass="logout" onClick={(e) => { this.props.setUnAuth(false)}}><IoIosLogOut/></Button></div>):null}
                 </h1>
-                <section className="main fix640">
-                    <h2 href="">Spending too much on Baleadas</h2>
-                    <h3>Top Higher Expense</h3>
-                    <h3>I spend more on</h3>
-                    <h3>Last Expense</h3>
-                    <h3>Total spent</h3>
-
-                    <h3></h3>
-                </section>
+                
+                <h2>{items}</h2>
+                    
                 <div>&nbsp;</div>
             </div>
         );

862d85af858cff4b4d8a1df8b660c8da6f86b23e
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-06-01 02:56:21 -0600

    Retoques en css y home loooged true && false

--- frontend/src/Components/Pages/Public/Home/Home.js
+++ frontend/src/Components/Pages/Public/Home/Home.js
@@ -23,6 +23,29 @@ export default class Home extends Component {
             </div>
           ) : null}
         </h1>
+
+        {!this.props.auth.logged ? (
+          <div className="home section-title">
+            <h2>Track your personal finance expenses quickly and easily</h2>
+
+            <p className="section-info">
+              Don't waste your time and money on paper, notebooks or complicated
+              applications.{" "}
+            </p>
+          </div>
+        ) : null}
+
+        {this.props.auth.logged ? (
+          <div className="home section-title">
+            <h2>Welcome! </h2>
+            <h3>{this.props.auth.user.email}</h3>
+
+            <p className="section-info">
+              Don't waste your time and money on paper, notebooks or complicated
+              applications.
+            </p>
+          </div>
+        ) : null}
       </div>
     );
   }
